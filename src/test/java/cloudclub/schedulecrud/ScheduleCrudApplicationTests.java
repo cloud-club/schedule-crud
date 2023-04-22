fe376aabad159b121343fea0ca88fecd4a52b3f2
fnzl54 <rnjscksdud1@naver.com>
2023-04-22 15:00:40 +0900

    feat : git init

---
+++ src/test/java/cloudclub/schedulecrud/ScheduleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudclub.schedulecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
